fa59c0876a4591f8271bdcfc8a5a4d1ac08ad26f
victor <contato@vfreitas.com>
2015-06-02 11:10:50 -0300

    beginning

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+// REQUIRED TO LOAD THE CLASSES.
 require 'lib/autoload.php';
 
-include 'lib/BBM/examples/download.php';
+
+// TEST INCLUDE, YOU CAN REMOVE THE COMMENTS AND TEST AS YOU WANT TO.
+//include 'lib/BBM/examples/download.php';
+//include 'lib/BBM/examples/purchase.php';
+//include 'lib/BBM/examples/catalog.php';
